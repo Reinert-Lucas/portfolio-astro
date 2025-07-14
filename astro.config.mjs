500e7eff28aa1dc7e654a6800fa71879089604c8
Lucas Iván Reinert <115881985+Reinert-Lucas@users.noreply.github.com>
2025-07-14 15:20:24 -0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Onest",
+        cssVariable: "--font-onest",
+        subsets: ["latin"],
+        weights: [
+          "100",
+          "200",
+          "300",
+          "400",
+          "500",
+          "600",
+          "700",
+          "800",
+          "900",
+        ],
+      },
+    ],
+  },
+});
